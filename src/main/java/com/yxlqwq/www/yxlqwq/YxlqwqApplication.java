e58d33cfc17a6fec71a416817c152150f047c32a
sr <17621751536@163.com>
2020-01-30 17:52:45 +0800

    frist commit

---
+++ src/main/java/com/yxlqwq/www/yxlqwq/YxlqwqApplication.java
@@ -0,0 +1,13 @@
+package com.yxlqwq.www.yxlqwq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxlqwqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YxlqwqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yxlqwq/www/yxlqwq/YxlqwqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxlqwq.www.yxlqwq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YxlqwqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
